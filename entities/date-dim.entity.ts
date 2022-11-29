60d812e840cc331974a7f5875b0f8cc956a2e47a
huyle-1851 <104247110+huyle-1851@users.noreply.github.com>
2022-11-29 22:14:11 +0700

    Delete entities directory

--- entities/date-dim.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-@Entity({ name: 'date_dim' })
-export class DateDimEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-
-    @Column({ type: 'date' })
-    date: Date;
-}
