5bb6bd3a60a04a9cf17a8afc4b50c439b129c39f
huyle-1851 <104247110+huyle-1851@users.noreply.github.com>
2022-11-29 22:13:02 +0700

    Add files via upload

---
+++ BackEnd/entities/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export * from './area-dim.entity';
+export * from './date-dim.entity';
+export * from './prize-dim.entity';
+export * from './province-dim.entity';
---
+++ BackEnd/entities/province-dim.entity.ts
@@ -0,0 +1,25 @@
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+@Entity({ name: 'province_dim' })
+export class ProvinceDimEntity {
+    @PrimaryGeneratedColumn({ type: 'int' })
+    id: number;
+
+    @Column({ type: 'nvarchar', length: 255 })
+    name: string;
+
+    @UpdateDateColumn()
+    updateAt: Date;
+
+    @Column({ name: 'dayexpiration_date' })
+    dayexpirationDate: Date;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createAt: Date;
+}
